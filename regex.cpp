4d37654b6b356fba33461607d6318ff8b15bde00
VasilLyubenov <vasko47@abv.bg>
2020-12-29 14:34:29 +0200

    Added a Min_Regex_Size funtion and fixed a typo
    mend

--- regex.cpp
+++ regex.cpp
@@ -9,14 +9,20 @@ int Size(const std::string Line)
   }
   return counter;
 }
+int Min_regex_Size(const std::string Regex) {
+	int counter = 0;
+	for (int i = 0; i < Size(Regex); i++)
+	{
+		if (Regex[i] == '\\') counter++;
+	}
+	return Size(Regex) - counter;
+}
 bool basic_search(const std::string Line, const std::string Regex){
-
   bool match = false;
   for (size_t line_pos = 0; line_pos <= Size(Line) - Min_regex_Size(Regex); line_pos++) {
-int curr_line = line_pos;
-  match = false;
+    int curr_line = line_pos;
+    match = false;
     for (size_t curr_reg = 0; curr_reg < Min_regex_Size(Regex); curr_reg++) {
-   if (Min_regex_Size(Regex) > (Size(Line) - line_pos)) return false;
       switch(Regex[curr_reg]){
         case '\\':{
           if(curr_reg == Size(Regex) - 1 ) {
@@ -24,11 +30,11 @@ int curr_line = line_pos;
           return false;
           }
           char next = Regex[curr_reg + 1];
-          if(next!='\\' && next !='\+' && next != '\*' && next != '\?' && next !='\^' && next!='\.'){
+          if (next!='\\' && next !='\+' && next != '\*' && next != '\?' && next !='\^' && next!='\.') {
             //std::cout<<"Missuese of the \'\\\' symbol!"
             return false;
           }
-          if(next==Line[curr_line]){
+          if (next==Line[curr_line]) {
             match = true;
             curr_line++;
             curr_reg++;
@@ -40,15 +46,15 @@ int curr_line = line_pos;
           break;
         }
 
-        case '\^':{
-          if(curr_reg!=0) {
+        case '\^': {
+          if (curr_reg!=0) {
             return false;
           }
           match = true;
         }
 
-        case '\.':{
-          if(curr_reg > Size(Line) - 1){
+        case '\.': {
+          if (curr_reg > Size(Line) - 1){
             break;
           }
           curr_line++;
@@ -56,8 +62,8 @@ int curr_line = line_pos;
           break;
         }
 
-        default :{
-          if(Regex[curr_reg]==Line[curr_line]){
+        default : {
+          if (Regex[curr_reg]==Line[curr_line]){
              curr_line++;
              match = true;
           }
@@ -82,7 +88,7 @@ int main(){
   std::cin>>Regex;
   MyFile.open(FileName,std::fstream::in);
   if(!MyFile.good()) {
-    std::cout<<"File was not found!";
+    std::cout<<"File could not open properly!";
     return -1;
   }
   while(getline(MyFile,Line))
